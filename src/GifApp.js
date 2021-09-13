165296f05c283087d071eedae9de1d60724c109f
Kneyder <Kevinneyderbutronm@gmail.com>
2021-09-13 16:04:59 -0400

    componente Gif

---
+++ src/GifApp.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const GifApp = props => {
+    return (
+        <div>
+            <h2>GifApp</h2>
+            <hr/>
+        </div>
+    )
+}
+
+GifApp.propTypes = {
+
+}
+
+export default GifApp
